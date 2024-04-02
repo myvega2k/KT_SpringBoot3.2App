0c7297193051ebcd596a8e38c1743a2726ad80ab
myvega2k <vega2k@kakao.com>
2024-04-02 16:44:03 +0900

    Entity add

---
+++ src/main/java/com/kt/myspringboot/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.kt.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
