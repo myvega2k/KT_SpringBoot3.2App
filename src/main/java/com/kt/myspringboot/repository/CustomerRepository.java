cd1219e7a2a6495e95a83279a03340f695c5654e
myvega2k <vega2k@kakao.com>
2024-04-02 17:13:50 +0900

    finder method add

--- src/main/java/com/kt/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/kt/myspringboot/repository/CustomerRepository.java
@@ -3,5 +3,11 @@ package com.kt.myspringboot.repository;
 import com.kt.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    //customerId 로 조회하는 query method
+    Optional<Customer> findByCustomerId(String id);
+    List<Customer> findByCustomerName(String name);
 }
