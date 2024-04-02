e3a3b1bffe8b7b28658f63a58da28ed4d240ea08
myvega2k <vega2k@kakao.com>
2024-04-02 13:17:32 +0900

    MyRunner update

--- src/main/java/com/kt/myspringboot/runner/MyRunner.java
+++ src/main/java/com/kt/myspringboot/runner/MyRunner.java
@@ -4,17 +4,29 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
 @Component
 public class MyRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("VM Argument : " + args.containsOption("foo"));
         System.out.println("Program Argument : " + args.containsOption("bar"));
 
+        //람다식 구문
         args.getOptionNames()//java.util.Set
                 .forEach(name -> System.out.println(name));  //Iterable
         /*
          forEach(Consumer)
          Consumer의 추상메서드 void accept(T t);
          */
+
+        //Anonymous Inner Class
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+
     }
 }
