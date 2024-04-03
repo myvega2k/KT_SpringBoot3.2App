f6173c56e444d93c2655332b68afcbee9f9f71d8
myvega2k <vega2k@kakao.com>
2024-04-03 11:41:58 +0900

    controller add

---
+++ src/main/java/com/kt/myspringboot/controller/UserRestController.java
@@ -0,0 +1,20 @@
+package com.kt.myspringboot.controller;
+
+import com.kt.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+}
