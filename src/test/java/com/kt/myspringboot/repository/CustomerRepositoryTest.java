7b1b4407192ad5f9fdde9eb29c264bb04039dba0
myvega2k <vega2k@kakao.com>
2024-04-03 09:46:25 +0900

    test case ㅕupdate

--- src/test/java/com/kt/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/kt/myspringboot/repository/CustomerRepositoryTest.java
@@ -22,10 +22,19 @@ class CustomerRepositoryTest {
         customer.setCustomerName("스프링");
         Customer addCustomer = customerRepository.save(customer);
         System.out.println(addCustomer.getCustomerId() + " " + addCustomer.getCustomerName());
+
         assertThat(addCustomer).isNotNull();
+        //assertj
         assertThat(addCustomer.getCustomerName()).isEqualTo("스프링");
+        //junit
+        assertEquals("스프링", addCustomer.getCustomerName());
+
         Optional<Customer> existCustomer = customerRepository.findByCustomerId(addCustomer.getCustomerId());
         assertThat(existCustomer).isNotEmpty();
+        if(existCustomer.isPresent()){
+            Customer customer1 = existCustomer.get();
+            assertThat(customer1.getCustomerName().equals("스프링"));
+        }
         Optional<Customer> notExistOptional = customerRepository.findByCustomerId("B001");
         assertThat(notExistOptional).isEmpty();
     }
