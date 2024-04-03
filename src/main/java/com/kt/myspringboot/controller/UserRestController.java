8b117e387c9ae1306f715871b53f5730a934c339
myvega2k <vega2k@kakao.com>
2024-04-03 14:05:22 +0900

    controller update

--- src/main/java/com/kt/myspringboot/controller/UserRestController.java
+++ src/main/java/com/kt/myspringboot/controller/UserRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -33,5 +34,10 @@ public class UserRestController {
                 () -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND)
         );
         return user;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
